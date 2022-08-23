e97f74679858a535cc14fb3cca1467052b93b702
okpalan <89138089+okpalan@users.noreply.github.com>
2022-08-23 14:14:34 +0100

    Update telnet-client.js

--- telnet-client.js
+++ telnet-client.js
@@ -1,45 +1,70 @@
 var net = require('net');
- 
+
 function Telnet() {
   this.queue = [];
   this.connected = false;
   this.promptReceived = false;
   this.telnetSocket = new net.Socket();
- 
-  this.telnetSocket.on('data', function(data) {
+
+  this.telnetSocket.on('data', function (data) {
     this.promptReceived = data.toString().trim().endsWith('$');
- 
+
     if (this.queue.length > 0) {
       this.write(this.queue.shift());
     }
   }.bind(this));
 }
- 
-Telnet.prototype.connect = function(options, callback) {
-  this.telnetSocket.connect(options, function() {
+
+Telnet.prototype.connect = function (options, callback) {
+  this.telnetSocket.connect(options, function () {
     this.connected = true;
     callback();
   }.bind(this));
 };
- 
-Telnet.prototype.exec = function(command, callback) {
+
+Telnet.prototype.exec = function (command, callback) {
   if (!this.connected) {
     throw new Error('Not connected');
   }
- 
+
   if (this.promptReceived) {
     this.write(command, callback);
   } else {
     this.queue.push(command);
   }
 };
- 
-Telnet.prototype.write = function(command, callback) {
-  this.telnetSocket.write(command + '\n', function() {
+
+Telnet.prototype.write = function (command, callback) {
+  this.telnetSocket.write(command + '\n', function () {
     if (callback) {
       callback();
     }
   });
 };
+
+Telnet.prototype.pause = function () {
+  this.telnetSocket.pause();
+};
+
+Telnet.prototype.on = function (event, callback) {
+  this.telnetSocket.on(event, function () {
+    callback();
+  }.bind(this));
+};
+
+Telnet.prototype.end = function () {
+  this.telnetSocket.end();
+};
+
+Telnet.prototype.prompt = function(prompt, callback) {
+  var data = '';
  
+  this.telnetSocket.on('data', function(chunk) {
+    data += chunk.toString();
+ 
+    if (data.includes(prompt)) {
+      callback(data);
+    }
+  }.bind(this));
+};
 module.exports = Telnet;
